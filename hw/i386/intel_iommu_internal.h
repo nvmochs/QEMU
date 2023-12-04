e48713b7fad66a0a0a5b6a9b7e0bd0707e2dbaec
Zhenzhong Duan <zhenzhong.duan@intel.com>
2023-12-04 20:25:17 +0800

    intel_iommu: ERRATA_772415 workaround
    On a system influenced by ERRATA_772415, IOMMU_HW_INFO_VTD_ERRATA_772415_SPR17
    is repored by IOMMU_DEVICE_GET_HW_INFO. Due to this errata, even the readonly
    range mapped on stage-2 page table could still be written.
    
    Reference from 4th Gen Intel Xeon Processor Scalable Family Specification
    Update, Errata Details, SPR17.
    
    [0] https://edc.intel.com/content/www/us/en/design/products-and-solutions/processors-and-chipsets/eagle-stream/sapphire-rapids-specification-update
    
    We utilize the new added IOMMUFD container/ioas/hwpt management framework in
    VTD. Add a check to create new VTDIOASContainer to hold RW-only mappings,
    then this VTDIOASContainer can be used as backend for device with
    ERRATA_772415. See below diagram for details:
    
          IntelIOMMUState
                 |
                 V
        .------------------.    .------------------.    .-------------------.
        | VTDIOASContainer |--->| VTDIOASContainer |--->| VTDIOASContainer  |-->...
        | (iommufd0,RW&RO) |    | (iommufd1,RW&RO) |    | (iommufd0,RW only)|
        .------------------.    .------------------.    .-------------------.
                 |                       |                              |
                 |                       .-->...                        |
                 V                                                      V
          .-------------------.    .-------------------.          .---------------.
          |   VTDS2Hwpt(CC)   |--->| VTDS2Hwpt(non-CC) |-->...    | VTDS2Hwpt(CC) |-->...
          .-------------------.    .-------------------.          .---------------.
              |            |               |                            |
              |            |               |                            |
        .-----------.  .-----------.  .------------.              .------------.
        | IOMMUFD   |  | IOMMUFD   |  | IOMMUFD    |              | IOMMUFD    |
        | Device(CC)|  | Device(CC)|  | Device     |              | Device(CC) |
        | (iommufd0)|  | (iommufd0)|  | (non-CC)   |              | (errata)   |
        |           |  |           |  | (iommufd0) |              | (iommufd0) |
        .-----------.  .-----------.  .------------.              .------------.
    
    Changed to pass VTDHostIOMMUDevice pointer to vtd_check_hdev() so errata
    could be saved.
    
    Suggested-by: Yi Liu <yi.l.liu@intel.com>
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>

--- hw/i386/intel_iommu_internal.h
+++ hw/i386/intel_iommu_internal.h
@@ -566,6 +566,7 @@ typedef struct VTDPASIDCacheEntry {
 typedef struct VTDIOASContainer {
     struct IOMMUFDBackend *iommufd;
     uint32_t ioas_id;
+    uint32_t errata;
     MemoryListener listener;
     QLIST_HEAD(, VTDS2Hwpt) s2_hwpt_list;
     QLIST_ENTRY(VTDIOASContainer) next;
@@ -690,5 +691,6 @@ typedef struct VTDHostIOMMUDevice {
     PCIBus *bus;
     uint8_t devfn;
     HostIOMMUDevice *hiod;
+    uint32_t errata;
 } VTDHostIOMMUDevice;
 #endif
